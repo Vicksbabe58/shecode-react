a17a5bc5d560619bbc9d81d173b18da1b64e5f8a
victory <victoryilikah58@gmail.com>
2023-03-03 13:47:01 +0100

    Added  axios integration so that it shows the weather of the city

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import WeatherSearch from "./WeatherSearch";
+import reportWebVitals from "./reportWebVitals";
+import { ColorRing } from "react-loader-spinner";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <div className="App">
+      <h1>Weather App</h1>
+      <ColorRing
+        visible={true}
+        height="80"
+        width="80"
+        ariaLabel="blocks-loading"
+        wrapperStyle={{}}
+        wrapperClass="blocks-wrapper"
+        colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+      />
+      <WeatherSearch />
+      <small>
+        <a
+          href="https://vicksbabe58.github.io/Shecodes-WeatherAPP-Final/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Open-source code
+        </a>
+        by Victory Ilikah
+      </small>
+    </div>
   </React.StrictMode>
 );
 
